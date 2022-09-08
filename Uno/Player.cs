723d2f95053e9f38bff04718f373f456a669e67d
Kenan Casey <kenancasey@gmail.com>
2022-09-08 10:15:36 -0500

    Update Player.cs

--- Uno/Player.cs
+++ Uno/Player.cs
@@ -3,9 +3,32 @@ namespace Uno
 {
 	public class Player
 	{
-		public Player()
+		public string Name { get; set; }
+        public List<Card> Hand { get; set; }
+
+
+        public Player()
 		{
+			Name = "";
+			Hand = new List<Card>();
 		}
-	}
+
+        public bool HasPlayableCard(Card card)
+		{
+			return false;
+		}
+
+        public Card GetFirstPlayableCard(Card card)
+		{
+			return new Card();
+		}
+
+        public Color MostCommonColor()
+		{
+			return Color.Blue;
+		}
+
+
+    }
 }
 
